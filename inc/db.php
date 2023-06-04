343fc2448dab71f4dc0e19b592087679aadd9087
m4mehdi <m3mehdi@hotmail.com>
2023-06-04 16:51:45 +0330

    git commit -m "userlist"

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "dbuser";
+$password = "123";
 $dbname="member";
 $db = new mysqli($servername, $username, $password, $dbname);
 
